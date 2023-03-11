523c37adee6d2817e1c57ba116b1f890d0b55b9f
belgherbi-lina <127565670+belgherbi-lina@users.noreply.github.com>
2023-03-11 20:59:34 +0100

    Add files via upload

---
+++ factoriel of positive number.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int factex01(int n ){
+	if(n==0 || n==1)return 1;
+	else return n*factex01(n-1);
+}
+main () 
+{ 
+	int number ;
+	do {
+	printf ("enter number");
+	scanf ("%d",&number);
+	}while(number<0);
+	printf ("the fact of this number is :%d",factex01(number));
+}
